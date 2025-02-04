de85fac73382001c9cbc662e2f1aee7bec1f6788
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-02-04 14:45:03 +0800

    Build: Add search results

--- admin_dashboard.js
+++ admin_dashboard.js
@@ -7,24 +7,8 @@ const announcementsPlaceHolder = document.getElementsByClassName("announcement-t
 
 var index = 0;
 
-// // Get each classname, capitalize the first letter
-// // of each word and then display on browser
-// projectNames.forEach((project) => {
-//     // all target elements has a "project" class
-//     // before its distinct class, so get the latest
-//     var title = project.classList[1].split("-")
-
-//     title = title.map((word) => {
-//         word = word[0].toUpperCase() + word.substring(1);
-//         return word
-//     }).join(" ")
-
-//     titlePlaceHolder[index].textContent = title;
-//     titlePlaceHolder[index].style['font-size'] = "1rem";
-//     titlePlaceHolder[index].style['font-weight'] = "bold";
-//     index++;
-// })
 const placeTitle = (titles, placeHolders, index = 0, fontSize = "1rem", fontWeight = "bold") => {
+    var data = [];
     titles.forEach((title) => {
 
         // all target elements has a general class
@@ -44,26 +28,53 @@ const placeTitle = (titles, placeHolders, index = 0, fontSize = "1rem", fontWeig
         placeHolders[index].style['font-size'] = fontSize;
         placeHolders[index].style['font-weight'] = fontWeight;
         index++;
+        data.push(obj)
     })
+
+    return data
 }
 
 
-placeTitle(projectNames, titlePlaceHolder, index);
 placeTitle(announcements, announcementsPlaceHolder, index, ".9rem")
 
-const getImg = document.querySelectorAll("img");
+const placeIcons = () => {
+    const getImg = document.querySelectorAll("img");
 
-getImg.forEach((img) => {
-    
-    if (img.alt === "S") {
-        img.src = "content-header-svg/bookmark-svgrepo-com.svg"
-    } else if (img.alt === "E") {
-        img.src = "content-header-svg/favorites-svgrepo-com.svg"
-    } else if (img.alt === "Y") {
-        img.src = "content-header-svg/link-minimalistic-svgrepo-com.svg"
-    }
-    img.width = "40"
-    img.height = "40"
+    getImg.forEach((img) => {
 
-})
+        if (img.alt === "S") {
+            img.src = "content-header-svg/bookmark-svgrepo-com.svg"
+        } else if (img.alt === "E") {
+            img.src = "content-header-svg/favorites-svgrepo-com.svg"
+        } else if (img.alt === "Y") {
+            img.src = "content-header-svg/link-minimalistic-svgrepo-com.svg"
+        }
+        img.width = "40"
+        img.height = "40"
 
+    })
+}
+
+placeIcons();
+
+const setDataList = () => {
+    const container = document.querySelector(".header-search")
+    const dataList = document.createElement("dataList")
+    dataList.setAttribute("id", "search-data")
+
+    const titles = placeTitle(projectNames, titlePlaceHolder, index);
+    titles.forEach((title) => {
+        let option = document.createElement("option")
+        option.setAttribute(`value`, title)
+        dataList.appendChild(option);
+    })
+
+    const searchBar = document.getElementById("search-bar");
+    searchBar.setAttribute("list", "search-data");
+
+    container.appendChild(dataList);
+
+}
+
+
+setDataList();
